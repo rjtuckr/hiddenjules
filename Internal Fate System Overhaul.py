2779d9822b5eac0c013eb07b76c89fb9e1bbb451
Jules <48770310+rjtuckr@users.noreply.github.com>
2019-07-06 20:18:24 -0700

    IFS Overhaul 2.2

--- Internal Fate System Overhaul.py
+++ Internal Fate System Overhaul.py
@@ -1,155 +1,15 @@
-from collections import namedtuple
 from random import *
 from tkinter import *
 from flukesys import fluke_system
+from bloodcraft import *
 
 # Written by Julie Tucker
 # rjtuckr@outlook.com
 
-# Attributes
-# Charisma (CH)
-# Constitution (CON)
-# Dexterity (DEX)
-# Intelligence (INT)
-# Luck (LCK)
-# Speed (SPD)
-# Strength (STR)
-# Wisdom (WIS)
-
-
-# Skills
-# Acrobatics (DEX)
-# Athletics (STR)
-# Block (SPD)
-# Chemistry (INT)
-# Control (WIS)
-# Craft (INT)
-# Destruction (WIS)
-# Disguise (CHA)
-# Engineering (INT)
-# Enhancement (WIS)
-# Enlightenment (CHA)
-# Escape (DEX)
-# Heavy Armor (CON)
-# Interaction (CHA)
-# Knowledge (INT)
-# Light Armor (SPD)
-# Medicine (WIS)
-# Melee (STR)
-# Perception (WIS)
-# Ranged (DEX)
-# Ride/Drive (DEX)
-# Security (INT)
-# Sense Motive (WIS)
-# Sleight of Hand (DEX)
-# Stealth (DEX)
-# Survival (WIS)
 
 class IFS:
-    def __init__(self, spin=None, pc=None, attribute=None, skill=None, difficulty=None, enemy=None, target=None,
-                 condition=None):
+    def __init__(self, pc=None, attribute=None, skill=None, difficulty=None, enemy=None, target=None, condition=None):
 
-        global Ho, Kinzin, Mignon, Raven, Sev, Vampyrate, name, hp, ac, ap, fortitude, will, reflex, charisma, \
-            constitution, dexterity, intelligence, luck, speed, strength, wisdom, disguise, enlightenment, \
-            interaction, heavy_armor, acrobatics, escape, ranged, ride, sleight_of_hand, stealth, chemistry, \
-            craft, engineer, knowledge, security, sense_motive, block, light_armor, athletics, melee, unarmed, \
-            control, destruction, enhancement, medicine, perception, survival, utility, Easy, Standard, Hard
-
-        # Levels of Difficulty
-
-        self.Easy = 8
-        self.Standard = 12
-        self.Hard = 16
-
-        self.Character = namedtuple("Character", "name hp ac ap fortitude will reflex charisma constitution dexterity "
-                                                 "intelligence luck speed strength wisdom")
-
-        self.Enemy = namedtuple("Enemy", "name hp ac ap fortitude will reflex charisma constitution dexterity "
-                                         "intelligence luck speed strength wisdom")
-
-        # MATHEMATICAL BREAKDOWN
-        # hp = 1.5 x pc[constitution] (score)
-
-        # light armor AC = 7 + pc[speed] (or pc[dexterity]) + pc[light_armor] (mod)
-        # heavy armor AC = 12 + pc[heavy_armor] + pc.constitution (mod)
-
-        # fortitude = 10 + pc[constitution] (mod)
-        # will = 10 + pc[wisdom] (mod)
-
-        # light armor reflex = 10 + pc[speed] (or pc[dexterity]) (mod)
-        # heavy armor reflex = 5 + pc[speed] (or pc[dexterity]) + (pc[heavy_armor] * .5) (mod)
-
-        # ap, or attack power, is d4 by default
-
-        # PCs and Enemies
-        self.Ho = self.Character("Ho", 30, 9, 4, 12, 13, 11, 20, 20, 10, 30, 20, 10, 20, 30)
-        self.Kinzin = self.Character("Kinzin", 45, 18, 4, 13, 12, 8, 10, 30, 20, 20, 10, 20, 30, 20)
-        self.Mignon = self.Character("Mignon", 30, 12, 4, 12, 13, 13, 10, 20, 30, 20, 10, 20, 20, 30)
-        self.Raven = self.Character("Raven", 15, 12, 4, 11, 12, 13, 20, 10, 30, 10, 20, 20, 30, 20)
-        self.Sev = self.Character("Sev", 30, 11, 4, 12, 13, 12, 30, 20, 10, 20, 10, 20, 20, 30)
-        self.Vampyrate = self.Enemy("Vampyrate", 15, 13, 4, 11, 11, 14, 15, 10, 40, 20, 30, 20, 10, 10)
-
-        # Global references
-
-        Easy = self.Easy
-        Standard = self.Standard
-        Hard = self.Hard
-
-        Ho = self.Ho
-        Kinzin = self.Kinzin
-        Mignon = self.Mignon
-        Raven = self.Raven
-        Sev = self.Sev
-        Vampyrate = self.Vampyrate
-
-        name = 0
-        hp = 1
-        ac = 2
-        ap = 3
-        fortitude = 4
-        will = 5
-        reflex = 6
-        charisma = 7        # Disguise, Enlightenment, Interaction
-        constitution = 8    # Heavy Armor
-        dexterity = 9       # Acrobatics, Escape, Ranged, Ride, Sleight of Hand, Stealth
-        intelligence = 10   # Chemistry, Craft, Engineer, Knowledge, Security, Sense Motive
-        luck = 11
-        speed = 12          # Block, Light Armor
-        strength = 13       # Athletics, Melee, Unarmed
-        wisdom = 14         # Control, Destruction, Enhancement, Medicine, Perception, Survival, Utility
-
-        disguise = "Disguise"
-        enlightenment = "Enlightenment"
-        interaction = "Interaction"
-        heavy_armor = "Heavy Armor"
-        acrobatics = "Acrobatics"
-        escape = "Escape"
-        ranged = "Ranged"
-        ride = "Ride"
-        sleight_of_hand = "Sleight of Hand"
-        stealth = "Stealth"
-        chemistry = "Chemistry"
-        craft = "Craft"
-        engineer = "Engineer"
-        knowledge = "Knowledge"
-        security = "Security"
-        sense_motive = "Sense Motive"
-        block = "Block"
-        light_armor = "Light Armor"
-        athletics = "Athletics"
-        melee = "Melee"
-        unarmed = "Unarmed"
-        control = "Control"
-        destruction = "Destruction"
-        enhancement = "Enhancement"
-        medicine = "Medicine"
-        perception = "Perception"
-        survival = "Survival"
-        utility = "Utility"
-
-        # Function variable setting (Find a more efficient way of doing this!)
-
-        self.spin = spin
         self.pc = pc
         self.attribute = attribute
         self.skill = skill
@@ -157,19 +17,6 @@ class IFS:
         self.enemy = enemy
         self.target = target
         self.condition = condition
-
-    # Fate's Wheel of Fate
-
-    # Ho - 1
-    # Kinzin - 2
-    # Mignon - 3
-    # Raven - 4
-    # Sev - 5
-
-    def wheel_of_fate(self):
-        self.spin = randint(1, 5)
-        print(self.spin)
-        return self.spin
 
     # Summoning the Improvised Damage System (IDS)
     # Enemy or PC mull roll d20 die and compare it against the target's AC
@@ -479,9 +326,6 @@ class IFS:
                 outcome_1 = int(randint(1, 15))
                 outcome = outcome_1 + luck_modifier + the_skill_modifier
 
-                #  Gonk's Fluke System
-                #  Gives each skill challenge a 15% chance for a lucky or unlucky chance encounter
-
                 # Success!
 
                 if outcome >= difficulty:
@@ -490,7 +334,11 @@ class IFS:
                     p3 = skill
                     p4 = " check!"
                     message = p1 + p2 + p3 + p4
+
+                    # Calculates the chance of a lucky encounter
+
                     check_for_fluke = fluke_system()
+
                     if check_for_fluke != "Lucky":
                         check_for_fluke = None
                     else:
@@ -505,7 +353,11 @@ class IFS:
                     p4 = " check..."
                     message = p1 + p2 + p3 + p4
                     diff = "Failed"
+
+                    # Calculates the chance of an unlucky encounter
+
                     check_for_fluke = fluke_system()
+
                     if check_for_fluke != "Unlucky":
                         check_for_fluke = None
                     else:
@@ -605,6 +457,7 @@ class IFS:
 
             def get_diff():
                 selected_diff = dvariable.get()
+
                 if selected_diff == "Easy":
                     selected_diff = Easy
 
